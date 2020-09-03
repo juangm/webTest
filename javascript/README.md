ca35a9dae2d93dffeca24ffcf1243754d3998c0b
juangm <juangarciamarinblog@gmail.com>
2020-09-03 11:55:44 +0200

    Add test case for playwright

--- javascript/README.md
+++ javascript/README.md
@@ -3,5 +3,21 @@
 ## Description
 
 - POC to check different javascript frameworks for E2E testing.
+- Follow the same TC described in the main repository.
+- List of frameworks to check/implement:
+  - puppeteer
+  - playwright
+  - cypress
 
-## :construction: In progress....
+## Installation
+
+- NodeJS
+- Run in the working directory `yarn install`
+
+## Puppeteer
+
+- Run puppeteer test: `yarn bare:pup`
+
+## Playwright
+
+- Run playwright test: `yarn play`
--- javascript/tests/playwright.ts
+++ javascript/tests/playwright.ts
@@ -1,8 +1,30 @@
 import { chromium } from 'playwright';
 
-(async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+const run = async () => {
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('http://localhost:5000/index.html');
+
+  // Create two items
+  await (await page.$('#item')).type('create repo');
+  await (await page.$('#add')).click();
+  await (await page.$('#item')).type('push first commit');
+  await (await page.$('#add')).click();
+
+  // Complete first task
+  let firstTask = await page.$('//ul[@id="todo"]/li[text()="create repo"]');
+  await (await firstTask.$('button.complete')).click();
+
+  // Complete second task
+  let secondTask = await page.$('//ul[@id="todo"]/li[text()="push first commit"]');
+  await (await secondTask.$('button.complete')).click();
+
+  // Delete both tasks
+  firstTask = await page.$('//ul[@id="completed"]/li[text()="create repo"]');
+  await (await firstTask.$('button.remove')).click();
+  secondTask = await page.$('//ul[@id="completed"]/li[text()="push first commit"]');
+  await (await secondTask.$('button.remove')).click();
   await browser.close();
-})();
+};
+
+run();
